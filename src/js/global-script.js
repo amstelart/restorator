b0ebb16c3d58b00605f62289e0f5011b307898c3
amstelart <amsteldroid@gmail.com>
2019-07-03 19:03:29 +0300

    first commit

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -1,7 +1,57 @@
 // Если на проекте jQuery
-// $( document ).ready(function() {
-//   // code
-// });
+$( document ).ready(function() {
+
+    $("#banner-carousel").owlCarousel({
+      items: 1,
+      nav: false,
+      dots: false,
+      loop: true,
+      center: true
+    });
+
+    $("#hits-carousel").owlCarousel({
+      items: 3,
+      nav: false,
+      dots: false,
+      loop: true,
+      center: true
+    });
+
+    $("#arrival-carousel").owlCarousel({
+      items: 3,
+      nav: false,
+      dots: false,
+      loop: true,
+      center: true
+    });
+
+    $("#brands-carousel").owlCarousel({
+      items: 4,
+      nav: false,
+      dots: false,
+      loop: true,
+      center: false
+    });
+
+    $("#photo-carousel").owlCarousel({
+      items: 4,
+      nav: false,
+      dots: false,
+      loop: true,
+      center: false
+    });
+
+    $("#reviews-carousel").owlCarousel({
+      items: 1,
+      margin: 30,
+      nav: false,
+      dots: true,
+      loop: true,
+      center: true,
+      dotsContainer: '.carousel__dots-wrap'
+    });
+
+});
 
 // Изоляция без jQuery
 // (function(){
